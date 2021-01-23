99f151156ea39b9ecca1ad48a36a600be3a6871a
irfan <hakimirfan90@gmail.com>
2021-01-23 22:48:46 +0700

    add tslint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("test");
+import express from "express";
+
+const app = express();
+const port = 8080 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hi!");
+});
+
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${port}`);
+});
